2491ba8c274391468cc7df0cd3795bb4d11cb8eb
bbruby <eugene.bbr@gmail.com>
2020-03-04 16:07:41 +0300

    for merge

--- juststart.rb
+++ juststart.rb
@@ -2,8 +2,5 @@ puts "HELLO WORLD"
 puts "VERSION 1"
 puts "change global config for user"
 puts "add new branch"
-<<<<<<< HEAD
 puts "add master diff"
-=======
 puts "newbranch diff"
->>>>>>> newbranch
